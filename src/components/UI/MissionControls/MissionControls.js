b40c89b396dae57f31ab4ac24232a6c1974fce76
Ilan Shemesh <ilan.shemesh@gmail.com>
2020-12-23 14:26:43 +0200

    MissionMonitor

--- src/components/UI/MissionControls/MissionControls.js
+++ src/components/UI/MissionControls/MissionControls.js
@@ -4,10 +4,21 @@ import classes from './MissionControls.module.css';
 const missionControls = props => {
     return (
         <div className={classes.Container}>
-            <i className={classes.MissionControl + " fas fa-play"}></i>
-            <i className={classes.MissionControl + " fas fa-pause"}></i>
-            <i className={classes.MissionControl + " fas fa-redo-alt"}></i>
-            <i className={classes.MissionControl + " fas fa-skull-crossbones"}></i>
+            <button className = {classes.MissionButton}> 
+                <i className={classes.MissionControl + " fas fa-play"}></i>
+            </button>
+           
+            <button className = {classes.MissionButton}>
+                <i className={classes.MissionControl + " fas fa-pause"}></i>
+            </button>
+
+            <button className = {classes.MissionButton}>
+                <i className={classes.MissionControl + " fas fa-redo-alt"}></i>
+            </button>
+
+            <button className = {classes.MissionButton}>
+                 <i className={classes.MissionControl + " fas fa-skull-crossbones"}></i>
+            </button>
         </div>
     );
 }
--- src/components/UI/MissionStatus/MissionStatus.js
+++ src/components/UI/MissionStatus/MissionStatus.js
@@ -4,15 +4,21 @@ const missionStatus = props => {
     return (
         <div className={classes.Container}>
             <div className={classes.Status}>
-                <i className={([classes.MissionStatus, classes.PartialSuccess]).join(' ') + " fas fa-check-circle"}></i>
+                <div className = {classes.CircularBorder}>
+                    <i className={([classes.MissionStatus, classes.PartialSuccess]).join(' ') + " fas fa-check-circle"}></i>
+                </div>
                 <h5>1-fails</h5>
             </div>
             <div className={classes.Status}>
-                <i className={([classes.MissionStatus, classes.Success]).join(' ') + " fas fa-cubes"}></i>
+                <div className = {classes.CircularBorder}>
+                    <i className={([classes.MissionStatus, classes.Success]).join(' ') + " fas fa-cubes"}></i>
+                </div>
                 <h5>4-fails</h5>
             </div>
             <div className={classes.Status}>
-                <i className={([classes.MissionStatus, classes.NotStarted]).join(' ') + " fas fa-running"}></i>
+                <div className = {[classes.CircularBorder, classes.CircularBorderRunIcon].join(' ')}>
+                    <i className={([classes.MissionStatus, classes.NotStarted]).join(' ') + " fas fa-running"}></i>
+                </div>
                 <h5>0-fails</h5>
             </div>
         </div>
